982e08b2899e8955d563273f58ce2843dee9c611
Nikolai Hesterberg <nlhester@ncsu.edu>
2020-04-28 23:00:30 +0200

    Added basic gui from designer

---
+++ src/net/hesterberg/budget/ui/BudgetGUI.java
@@ -0,0 +1,14 @@
+package net.hesterberg.budget.ui;
+
+import javax.swing.*;
+
+public class BudgetGUI {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+}
